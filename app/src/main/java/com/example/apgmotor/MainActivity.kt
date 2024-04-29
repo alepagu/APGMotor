d4b9f8e427b6bb97ebfd2b64d1bcb938a2498408
alepagu <padilla.guale22@cordoba.salesianos.edu>
2024-04-29 20:57:05 +0200

    First Commit to add the logo image

--- app/src/main/java/com/example/apgmotor/MainActivity.kt
+++ app/src/main/java/com/example/apgmotor/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Login("Android")
                 }
             }
         }
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Login(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -39,8 +39,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LoginPreview() {
     APGMotorTheme {
-        Greeting("Android")
+        Login("Android")
     }
 }
